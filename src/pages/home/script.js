4e9e0823e2c088b46aa88dace4e31e4cd58b8607
Fabiano Mendonça <mendonca.biano@gmail.com>
2023-08-25 17:42:15 -0300

    :art: style: formatação dos documentos

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -1,81 +1,80 @@
 const reloadPage = new Event("reload");
-const mainContent = document.querySelector('#main');
+const mainContent = document.querySelector("#main");
 
 // função para remover o modal
-function modalDismiss(modalName){
-    let modal = bootstrap.Modal.getInstance(document.querySelector(modalName))
-    modal.hide();
+function modalDismiss(modalName) {
+  let modal = bootstrap.Modal.getInstance(document.querySelector(modalName));
+  modal.hide();
 }
-function disabledAddTeam(){
-    let buttonadd = document.querySelector('#addNewTeam').classList.add('disabled')
+function disabledAddTeam() {
+  let buttonadd = document
+    .querySelector("#addNewTeam")
+    .classList.add("disabled");
 }
 
-// submeter formulario de adicionar um novo time 
-function submitFormAddTeam(){
-    if(localStorage.getItem('index') >= 4){
-        modalDismiss('#modalAddTeam');
-        disabledAddTeam();
-    }else{
-        let index;
-        localStorage.getItem('index') == null 
-            ? index = 0 
-            : index = localStorage.getItem('index');
-        
-        let color = document.querySelector('#colorInput').value;
-        let name = document.querySelector('#nameInput').value;
-        localStorage.setItem(index, JSON.stringify({ "nome" : name,
-                                                     "cor" : color}) ); 
-        index++;
-        localStorage.setItem('index',index);
-        if(index == 4){
-            disabledAddTeam()
-        }
-        form = document.querySelector('#formAddTeam');
-        form.reset();
-        modalDismiss('#modalAddTeam');
-        main.dispatchEvent(reloadPage);
+// submeter formulario de adicionar um novo time
+function submitFormAddTeam() {
+  if (localStorage.getItem("index") >= 4) {
+    modalDismiss("#modalAddTeam");
+    disabledAddTeam();
+  } else {
+    let index;
+    localStorage.getItem("index") == null
+      ? (index = 0)
+      : (index = localStorage.getItem("index"));
+
+    let color = document.querySelector("#colorInput").value;
+    let name = document.querySelector("#nameInput").value;
+    localStorage.setItem(index, JSON.stringify({ nome: name, cor: color }));
+    index++;
+    localStorage.setItem("index", index);
+    if (index == 4) {
+      disabledAddTeam();
     }
+    form = document.querySelector("#formAddTeam");
+    form.reset();
+    modalDismiss("#modalAddTeam");
+    main.dispatchEvent(reloadPage);
+  }
 }
 
 // construir um evento para atualizar a pagina
 mainContent.addEventListener(
-    "reload",
-    (e) => {
-    //   console.log('uhum')
-        let index = localStorage.getItem('index');
-        let teams = []
-        if(index){
-            for (let i=0; i<index;i++){
-                teams.push(localStorage.getItem(i))
-            }
-            rebuildteams(teams);
-        }
-    },
-    false,
+  "reload",
+  (e) => {
+    let index = localStorage.getItem("index");
+    let teams = [];
+    if (index) {
+      for (let i = 0; i < index; i++) {
+        teams.push(localStorage.getItem(i));
+      }
+      rebuildteams(teams);
+    }
+  },
+  false
 );
 
 // função para contruir os times
-function rebuildteams(teams){
-    console.log(teams)
-    for(let i=0; i<teams.length; i++){
-        let teamElement = document.querySelector(`#time${i}`)
-        teamElement.style.color = '#FFFF'
-        let nameTeam = teamElement.querySelector('#nameTeam')
-        let colorTeam = teamElement.querySelector('#colorTeam')
-        let props = JSON.parse(teams[i])
-        nameTeam.textContent = props.nome;
-        colorTeam.style = `background:${props.cor}`
-
-    }
-
+function rebuildteams(teams) {
+  console.log(teams);
+  for (let i = 0; i < teams.length; i++) {
+    let teamElement = document.querySelector(`#time${i}`);
+    teamElement.style.color = "#FFFF";
+    let nameTeam = teamElement.querySelector("#nameTeam");
+    let colorTeam = teamElement.querySelector("#colorTeam");
+    let props = JSON.parse(teams[i]);
+    nameTeam.textContent = props.nome;
+    colorTeam.style = `background:${props.cor}`;
+  }
 }
 
-function clearLocalStorage(){
-    localStorage.clear();
-    location.reload()
+function clearLocalStorage() {
+  localStorage.clear();
+  location.reload();
 }
 
+// conteudo principal
 main.dispatchEvent(reloadPage);
-if(localStorage.getItem('index') >= 4){
-    disabledAddTeam()
+if (localStorage.getItem("index") >= 4) {
+  disabledAddTeam();
 }
