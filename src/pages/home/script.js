f48fa9f9e44865da0860831a48d62ce6ef451293
Fabiano Mendonça <mendonca.biano@gmail.com>
2023-08-25 17:08:30 -0300

    :sparkles: feat: carregar times do localstorage

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -17,11 +17,13 @@ function submitFormAddTeam(){
         localStorage.getItem('index') == null 
             ? index = 0 
             : index = localStorage.getItem('index');
-        index++;
-        localStorage.setItem('index',index);
+        
         let color = document.querySelector('#colorInput').value;
         let name = document.querySelector('#nameInput').value;
-        localStorage.setItem(index, JSON.stringify(`${name}:${color}`) ); 
+        localStorage.setItem(index, JSON.stringify({ "nome" : name,
+                                                     "cor" : color}) ); 
+        index++;
+        localStorage.setItem('index',index);
         form = document.querySelector('#formAddTeam');
         form.reset();
         modalDismiss('#modalAddTeam');
@@ -33,7 +35,36 @@ function submitFormAddTeam(){
 mainContent.addEventListener(
     "reload",
     (e) => {
-      console.log('uhum')
+    //   console.log('uhum')
+        let index = localStorage.getItem('index');
+        let teams = []
+        if(index){
+            for (let i=0; i<index;i++){
+                teams.push(localStorage.getItem(i))
+            }
+            rebuildteams(teams);
+        }
     },
     false,
 );
+
+// função para contruir os times
+function rebuildteams(teams){
+    console.log(teams)
+    for(let i=0; i<teams.length; i++){
+        let teamElement = document.querySelector(`#time${i}`)
+        teamElement.style.color = '#FFFF'
+        let nameTeam = teamElement.querySelector('#nameTeam')
+        let colorTeam = teamElement.querySelector('#colorTeam')
+        let props = JSON.parse(teams[i])
+        nameTeam.textContent = props.nome;
+        colorTeam.style = `background:${props.cor}`
+
+    }
+
+}
+
+function clearLocalStorage(){
+    localStorage.clear();
+    main.dispatchEvent(reloadPage);
+}
